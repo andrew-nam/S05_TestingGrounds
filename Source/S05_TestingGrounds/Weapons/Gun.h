6fbe858f3dbe64d5ef82105bb998b3d299269096
dan6ra <dan6ra@virginia.edu>
2019-09-29 11:50:14 -0700

    Configured Gun class to respond to OnFire event from FirstPersonCharacter class.

--- Source/S05_TestingGrounds/Weapons/Gun.h
+++ Source/S05_TestingGrounds/Weapons/Gun.h
@@ -46,11 +46,11 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	/** Fires a projectile. */
-	void OnFire();
-
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	/** Fires a projectile. */
+	void OnFire();
 
 };
